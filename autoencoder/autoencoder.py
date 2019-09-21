ba3c5ee9ae845592bb9de295f23512fef3f237c9
PersevereStudy <conquerzheng@163.com>
2019-09-21 09:54:09 +0800

    [ADD] Add some functions of AutoEncoder Class for mimo_ofdm_im. The Quasci-statci or Rayleigh channel can be chosen.

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -104,6 +104,8 @@
 """
 以上是Autoencoder的基本结构，现在最主要的是要产生数据，MIMO-OFDM-IM的数据
 """
+
+
 #创建MIMO-OFDM-IM系统模型
 # k = 3 # Number of information bits per channel usage
 M = 2
@@ -125,6 +127,7 @@ assert nTx == nRx, "Number of Transmit and Receive antennas is not equal"
 
 # In[ ]:
 import numpy as np
+import tensorflow as tf
 
 class AutoEncoder(object):
     def __init__(self, M, n, mt, mr, N, K, seed=None, filename=None):
@@ -157,6 +160,186 @@ class AutoEncoder(object):
         #     self.load(filename)
         return
 
+    def create_graph(self):
+        '''This function creates the computation graph of the autoencoder'''
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            # Comment out if you want to set the RNG seed
+            # tf.set_random_seed(self.seed)
+
+            # Placeholder which defines the input of the AE
+            batch_size = tf.placeholder(tf.int32, shape=())
+
+            # Create Channelmatrix
+            hc, h_resh = self.channel_matrix()
+
+            # Transmitter
+            s = tf.random_uniform(shape=[batch_size], minval=0, maxval=self.T, dtype=tf.int64)  # 产生了0到T-1的十进制信源
+            x, x_plot, vars_enc = self.encoder(s, h_resh)
+
+            # Channel
+            noise_std = tf.placeholder(tf.float32, shape=())
+            y = self.channel(x, hc, noise_std)
+
+            # Receiver RTN
+            yc_rtn, h_hat_dec, _ = self.predecode(y, h_resh)
+            s_hat, vars_dec = self.decoder(yc_rtn, h_resh)
+
+            # Loss function
+            cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=s, logits=s_hat)
+
+            # Performance metrics
+            preds = tf.nn.softmax(s_hat)
+            s_hat_bit = tf.argmax(preds, axis=1)
+            correct_predictions = tf.equal(s_hat_bit, s)
+            accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+            bler = 1 - accuracy
+
+            # Optimizer
+            lr = tf.placeholder(tf.float32, shape=())  # We can feed in any desired learning rate for each step
+            train_op = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+
+            # Code lines which need to be implemented to monitor AE behavior through Tensorboard
+            with tf.name_scope('performance'):
+                tf.summary.scalar('loss', cross_entropy)
+                tf.summary.scalar('bler', bler)
+
+            train_vars = tf.trainable_variables()  # tf.get_variable('enc_2/kernel')
+            train_v = [vars_enc, vars_dec]
+
+            merged = tf.summary.merge_all()
+
+            # References to graph variables we need to access later
+            self.vars = {
+                'accuracy': accuracy,
+                'batch_size': batch_size,
+                'bler': bler,
+                'cross_entropy': cross_entropy,
+                'init': tf.global_variables_initializer(),
+                'lr': lr,
+                'noise_std': noise_std,
+                'train_op': train_op,
+                's': s,
+                's_hat': s_hat,
+                'x': x,
+                'h': hc,
+                'y': y,
+                'x_plot': x_plot,
+                'hc': hc,
+                's_hat_bit': s_hat_bit,
+                'preds': preds,
+                'merged': merged,
+                'train_vars': train_vars,
+                'train_v': train_v,
+                'yc_rtn': yc_rtn,
+                'h_hat_dec': h_hat_dec,
+                'corr_preds': correct_predictions
+            }
+            self.saver = tf.train.Saver()
+        return
+
+    def channel_matrix(self):
+        '''The Channel Matrix Calculation (Rayleigh Fading Channel)'''
+
+        # Define standard deviation as Rayleigh
+        h_std_r = np.sqrt(0.5)
+        h_mean_r = 0
+        h_std_i = np.sqrt(0.5)
+        h_mean_i = 0
+
+        # Calculate channel symbols
+        hr = tf.random_normal([self.mt, self.mr], mean=h_mean_r, stddev=h_std_r, dtype=tf.float32)
+        hi = tf.random_normal([self.mt, self.mr], mean=h_mean_i, stddev=h_std_i, dtype=tf.float32)
+        hc = tf.complex(hr, hi)
+
+        # Channel in real notation
+        h = tf.concat([tf.real(hc), tf.imag(hc)], axis=1)
+        h_resh = tf.reshape(h, shape=[1, -1])
+
+        return hc, h_resh
+
+    def encoder(self, input, h):
+        '''The transmitter'''
+
+        # Embedding Layer
+        with tf.name_scope('enc_1'):
+            # Embedding Matrix
+            W = self.weight_variable((self.T, self.T))
+            # Embedding Look-Up and ELU-Activation
+            x = tf.nn.elu(tf.nn.embedding_lookup(W, input), name='enc_1')
+            # Store layer parameters to monitor them in Tensorboard
+            var_enc_1 = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'enc_1')
+
+        # Hidden Layer
+        with tf.name_scope('enc_2'):
+            # Dense Layer
+            x = tf.layers.dense(x, self.mt * self.M, activation=tf.nn.leaky_relu, name='enc_2')
+            # Store layer parameters to monitor them in Tensorboard
+            var_enc_2 = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'enc_2')
+
+        # Hidden Layer
+        with tf.name_scope('enc_3'):
+            # Dense Layer
+            x = tf.layers.dense(x, 2 * self.mt * self.mt, activation=tf.nn.leaky_relu, name='enc_3')
+            # Store layer parameters to monitor them in Tensorboard
+            var_enc_3 = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'enc_3')
+
+        # Output Layer
+        with tf.name_scope('enc_4'):
+            # Dense Layer
+            x = tf.layers.dense(x, 2 * self.mt, activation=None, name='enc_4')
+            # Store layer parameters to monitor them in Tensorboard
+            var_enc_4 = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'enc_4')
+
+        # Concat all summaries of the layer parameters
+        vars = [var_enc_1, var_enc_2, var_enc_3, var_enc_4]
+
+        # Real to complex transformation
+        x = tf.reshape(x, shape=[-1, 2])
+        x = tf.reshape(tf.complex(x[::, 0], x[::, 1]), shape=[-1, 1])
+
+        # Power Normalization (Comment out the one you want to use)
+
+        # Average power normalization over all
+        norma = tf.sqrt(tf.reduce_mean(tf.square(tf.abs(x)), axis=0))
+        x = x / tf.cast(norma, dtype=tf.complex64)
+        '''
+        # Power Normalization = 1
+        norma = tf.reciprocal(tf.sqrt(tf.square(tf.abs(x))))
+        x = tf.multiply(x, tf.cast(norma, dtype=tf.complex64))
+        '''
+
+        # Antenna Mapping
+        x = tf.reshape(x, shape=[-1, self.mt])
+
+        '''
+        # Average power normalization over each antenna
+        norma = tf.sqrt(tf.reduce_mean(tf.square(tf.abs(x)), axis=0))
+        x = x / tf.cast(norma, dtype=tf.complex64)
+        '''
+
+        # x_plot needs to be reshaped sometimes for plotting purposes
+        x_plot = x
+
+        return x, x_plot, vars
+
+    def channel(self, xc, hc, noise_std):
+        '''The channel'''
+
+        # Channel Matrix Multiplication
+        yc = tf.matmul(xc, hc)
+
+        # Noise
+        nr = tf.random_normal(tf.shape(yc), mean=0.0, stddev=(noise_std / np.sqrt(2)), dtype=tf.float32)
+        ni = tf.random_normal(tf.shape(yc), mean=0.0, stddev=(noise_std / np.sqrt(2)), dtype=tf.float32)
+        nc = tf.complex(nr, ni)
+        yc = tf.add(yc, nc)
+
+        return yc
+
+    ####################
+    #以下是常规的mimo_ofdm_im模型
+    ####################
     def source_bits(self):
         q = self.index_bits + self.info_bits
         # print(q)
@@ -229,16 +412,34 @@ class AutoEncoder(object):
             source_bits_for_antenas[i_tx] = bits
             tx_symbols = self.ofdm_im(bit_index, bit_info)
             symbols_for_antenas[i_tx] = tx_symbols #每一行代表1个发射天线的符号，每一列代表每个子载波
+        symbols_for_Tx = np.array(symbols_for_antenas)
+        return source_bits_for_antenas,symbols_for_Tx
 
-        return source_bits_for_antenas,symbols_for_antenas
+    def channel_gen(self,channel_type=1):
+        channel_h = np.zeros((self.nRx,self.nTx))
+        if channel_type == 0: #quasi-static channel
+            channel_h = np.array([[1,0],[0,1]])
+        else:
+            channel_h = np.sqrt(1 / 2) * (np.random.randn(self.nRx,self.nTx) + 1j * np.random.randn(self.nRx,self.nTx))
+            
+        return channel_h
 
-    def channel_gen(self,channel_type):
-        pass
+    def pass_channel(self,channel_h,symbols_for_Tx):
+        "因为要传输N个子载波"
+        symbols_for_Rx = np.zeros((self.nRx,self.N),dtype=complex)
+        for i in range(self.N):
+            symbols_for_Rx[:,i] = np.dot(channel_h,symbols_for_Tx[:,i])
+        return symbols_for_Rx #每一行代表1个接收天线接收到的符号，每一列代表每个子载波
 
-    def pass_channel(self):
-        pass
+    def equalizer(self,symbols_for_Rx,channel_h): #均衡
+        Yg_bar = np.zeros((self.nRx,self.N),dtype=complex)
+        h_inv = np.linalg.inv(channel_h)
+        for i in range(self.N):
+            Yg = symbols_for_Rx[:,i].reshape(-1, 1)
+            Yg_bar[:,i] = h_inv.dot(Yg)[:,0] #矩阵乘法之后的尺寸为(2,1)不能放进Yg_bar[:,i],故取出第一列后赋值
+        return Yg_bar #每一行代表1个接收天线接收到的符号，每一列代表每个子载波
 
-# ae = AutoEncoder(M, n, nTx, nRx, N, K)
+ae = AutoEncoder(M, n, nTx, nRx, N, K)
 # # bits,bit_index,bit_info = ae.source_bits()
 # # print(bits,bit_index,bit_info)
 # #
@@ -248,5 +449,12 @@ class AutoEncoder(object):
 # # tx_symbols = ae.ofdm_im(bit_index,bit_info)
 # # print(tx_symbols)
 #
-# source_bits,mimo_symbols = ae.mimo()
-# print(source_bits,'\n',mimo_symbols)
+source_bits,mimo_symbols = ae.mimo()
+print(source_bits,'\n',mimo_symbols)
+# H = ae.channel_gen(channel_type=0)
+H = ae.channel_gen()
+print(H)
+symbols_for_Rx = ae.pass_channel(H,mimo_symbols)
+print(symbols_for_Rx)
+Yg_bar = ae.equalizer(symbols_for_Rx,H)#均衡
+print(Yg_bar)
